982b4b5270d0e584c0b20e004d21a1b1c5be9e33
yunjey <yunjey47@naver.com>
2016-12-30 17:06:30 +0900

    downlad svhn

---
+++ download.sh
@@ -0,0 +1,5 @@
+mkdir -p svhn
+wget -O svhn/train_32x32.mat http://ufldl.stanford.edu/housenumbers/train_32x32.mat
+wget -O svhn/test_32x32.mat http://ufldl.stanford.edu/housenumbers/test_32x32.mat
+
+

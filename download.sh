7aa73d31a20423f48ec980e962479aa1fba33199
yunjey <yunjey47@naver.com>
2017-01-22 05:50:25 +0900

    download svhn dataset

--- download.sh
+++ download.sh
@@ -1,7 +1,6 @@
 mkdir -p mnist
 mkdir -p svhn
+
 wget -O svhn/train_32x32.mat http://ufldl.stanford.edu/housenumbers/train_32x32.mat
 wget -O svhn/test_32x32.mat http://ufldl.stanford.edu/housenumbers/test_32x32.mat
 wget -O svhn/extra_32x32.mat http://ufldl.stanford.edu/housenumbers/extra_32x32.mat
-
-

249b708c10dbe579ea75f9d91b71f5fd903de41a
yunjey <yunjey47@naver.com>
2017-01-22 18:03:21 +0900

    add code for creating directories

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ from solver import Solver
 
 flags = tf.app.flags
 flags.DEFINE_string('mode', 'train', "'pretrain', 'train' or 'eval'")
+flags.DEFINE_string('model_save_path', 'model', "directory for saving the model")
+flags.DEFINE_string('sample_save_path', 'sample', "directory for saving the sampled images")
 FLAGS = flags.FLAGS
 
 def main(_):
     
     model = DTN(mode=FLAGS.mode, learning_rate=0.0003)
-    solver = Solver(model, batch_size=100, pretrain_iter=5000, train_iter=2000, sample_iter=100, 
-                    svhn_dir='svhn', mnist_dir='mnist', log_dir='logs', model_save_path='model')
+    solver = Solver(model, batch_size=100, pretrain_iter=20000, train_iter=2000, sample_iter=100, 
+                    svhn_dir='svhn', mnist_dir='mnist', model_save_path=FLAGS.model_save_path, sample_save_path=FLAGS.sample_save_path)
+    
+    # create directories if not exist
+    if not tf.gfile.Exists(FLAGS.model_save_path):
+        tf.gfile.MakeDirs(FLAGS.model_save_path)
+    if not tf.gfile.Exists(FLAGS.sample_save_path):
+        tf.gfile.MakeDirs(FLAGS.sample_save_path)
     
     if FLAGS.mode == 'pretrain':
         solver.pretrain()
